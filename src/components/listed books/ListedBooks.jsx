516ad3fcfe716d8a0991e69ebfa2de09635dcac5
Md. Kamrul Islam <join.kamrul@gmail.com>
2024-03-26 14:10:10 +0600

    drop down menu design done

--- src/components/listed books/ListedBooks.jsx
+++ src/components/listed books/ListedBooks.jsx
@@ -1,7 +1,23 @@
+import { IoIosArrowDropdown } from "react-icons/io";
+
 const ListedBooks = () => {
     return (
-        <div>
-            {/* <h2>listed_books</h2> */}
+        <div className="mt-9">
+
+            {/* heading */}
+            <h2 className="w-full bg-[#1313130D] rounded-2xl py-8 text-center text-[#131313] text-[28px] font-bold mb-8">Books</h2>
+
+            {/* drop down menu */}
+            <div className="flex justify-center">
+            <details className="dropdown">
+                <summary className="w-52 m-1 btn text-white font-semibold text-lg bg-[#23BE0A] hover:bg-transparent hover:border-[#23BE0A] hover:text-[#23BE0A]">Sort By <IoIosArrowDropdown /></summary>
+                <ul className="p-2 shadow menu dropdown-content z-[1] rounded-box w-52 bg-base-200">
+                    <li><a>Item 1</a></li>
+                    <li><a>Item 2</a></li>
+                </ul>
+            </details>
+            </div>
+
         </div>
     );
 };
