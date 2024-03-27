e44bec22635a30760b2560aa0b17e8e675bf71a3
Md. Kamrul Islam <join.kamrul@gmail.com>
2024-03-27 22:35:31 +0600

    listed books section update

--- src/components/listed books/ListedBooks.jsx
+++ src/components/listed books/ListedBooks.jsx
@@ -21,11 +21,11 @@ const ListedBooks = () => {
 
             {/* tab */}
             <div role="tablist" className="tabs tabs-lifted text-lg">
-                <input type="radio" name="my_tabs_2" role="tab" className="tab" aria-label="Tab 1" />
-                <div role="tabpanel" className="tab-content bg-base-100 border-base-300 rounded-box p-6">Tab content 1</div>
+                <input type="radio" name="my_tabs_2" role="tab" className="tab" aria-label="Read Books" />
+                <div role="tabpanel" className="tab-content bg-base-100 border-base-300 rounded-box p-6">Read Books</div>
 
-                <input type="radio" name="my_tabs_2" role="tab" className="tab" aria-label="Tab 2" checked />
-                <div role="tabpanel" className="tab-content bg-base-100 border-base-300 rounded-box p-6">Tab content 2</div>
+                <input type="radio" name="my_tabs_2" role="tab" className="tab" aria-label="Wishlist Books" checked />
+                <div role="tabpanel" className="tab-content bg-base-100 border-base-300 rounded-box p-6">Wishlist Books</div>
             </div>
 
         </div>
